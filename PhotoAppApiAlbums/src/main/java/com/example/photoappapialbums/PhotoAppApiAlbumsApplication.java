af958dbb6afc1d989c552087d981fc422fc8bae5
Eren Adıgüzel <erenadiguzel96@gmail.com>
2022-07-17 20:37:55 +0300

    add photo app

---
+++ PhotoAppApiAlbums/src/main/java/com/example/photoappapialbums/PhotoAppApiAlbumsApplication.java
@@ -0,0 +1,13 @@
+package com.example.photoappapialbums;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhotoAppApiAlbumsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PhotoAppApiAlbumsApplication.class, args);
+    }
+
+}
